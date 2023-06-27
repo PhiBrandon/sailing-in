7708068041e250b708117246d5ca7117c8eb598c
Brandon phillips <thedefinedone@gmail.com>
2023-06-27 05:25:28 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-black text-white h-screen flex flex-col justify-center items-center">
+        <img src="/img/comrade.jpg" alt="For The Way of Man" />
+        <h1>Comrade Coin</h1>
+        <button class="p-4 px-10 mt-2 hover:bg-white hover:text-black border-white border rounded-md">Buy Now!</button>
+    </div>
+</template>
